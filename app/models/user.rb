a9433d0c36f4ce66d7210e978e82dec4f5b4ce29
Charles Arciniega <charlesarcin@gmail.com>
2014-09-15 21:36:46 -0700

    password encryption added to user model. Used as a callback before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true, confirmation: true
   validates :email, presence: true, uniqueness: true
+
+  before_save :encrypt_password
+
+  private
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
 end
