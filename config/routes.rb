68b5ce979fb844886973fd2849feedd6c56ea58c
Charles Arciniega <charlesarcin@gmail.com>
2014-09-15 21:49:17 -0700

    add login form and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "users#index"
   get 'sign_up' => "users#new", as: "sign_up"
+  get 'log_in' => "sessions#new", as: 'log_in'
+  resources :sessions
   resources :users
 end
