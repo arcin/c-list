e152d628ab8ff4dbc4a87779d7c0f2c12c8fe6c8
Charles Arciniega <charlesarcin@gmail.com>
2014-09-16 02:25:09 -0700

    created index action and view. set page route to posts#index and added resources for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: "users#index"
+  root to: "posts#index"
   get 'sign_up' => "users#new", as: "sign_up"
   get 'log_in' => "sessions#new", as: 'log_in'
   get 'log_out' => "sessions#destroy", as: 'log_out'
+  resources :posts
   resources :sessions
   resources :users
 end
