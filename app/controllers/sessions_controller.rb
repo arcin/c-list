173b49247a5c2c3614155a7ba0e5760e5449ceca
Charles Arciniega <charlesarcin@gmail.com>
2014-09-15 21:59:43 -0700

    add user authentication using model authenticate method and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Successfully logged in!"
+    else
+      flash.now.alert = "Username or Password is invalid!"
+      render "new"
+    end
   end
 end
