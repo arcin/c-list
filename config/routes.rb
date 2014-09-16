b5d405c7eefbbb16d42548cace63d86fb9b433b8
Charles Arciniega <charlesarcin@gmail.com>
2014-09-15 22:04:27 -0700

    adds log_out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "users#index"
   get 'sign_up' => "users#new", as: "sign_up"
   get 'log_in' => "sessions#new", as: 'log_in'
+  get 'log_out' => "sessions#destroy", as: 'log_out'
   resources :sessions
   resources :users
 end
