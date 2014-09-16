a8b17dcbccab60892e47432bd9b93466b136a37c
Charles Arciniega <charlesarcin@gmail.com>
2014-09-16 02:32:23 -0700

    user must be signed in to create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    if session[:user_id]
+      @user = User.new
+    else
+      redirect_to posts_path, notice: "You must be signed in to create a Post!"
+    end
+  end
 end
