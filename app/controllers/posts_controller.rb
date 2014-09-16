e5ff2ef3ebb7b0cf92aaee01135af99fff857bd3
Charles Arciniega <charlesarcin@gmail.com>
2014-09-16 03:51:19 -0700

    create form for creating posts and controller actions for it as well. Posts can only be created if users are logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,23 @@ class PostsController < ApplicationController
 
   def new
     if session[:user_id]
-      @user = User.new
+      @post = Post.new
     else
       redirect_to posts_path, notice: "You must be signed in to create a Post!"
     end
+  end
+
+  def create
+    post = current_user.posts.build(post_params)
+    if post.save
+      redirect_to posts_path, notice: "your post has been successfully created!"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :price)
   end
 end
