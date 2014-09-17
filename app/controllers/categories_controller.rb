08373f05f9097331855b98df5e8056132541426b
Charles Arciniega <charlesarcin@gmail.com>
2014-09-17 16:00:33 -0700

    Add logic for category creation and only allow creation if a user is logged in

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,9 +7,30 @@ class CategoriesController < ApplicationController
   def show
     @posts = @category.posts
   end
+
+  def new
+    if session[:user_id]
+      @category = Category.new
+    else
+      redirect_to categories_path, alert: "You must be logged in to create a category"
+    end
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "Category Successfully created!"
+    else
+      render "new"
+    end
+  end
   private
     def set_category
       @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
     end
 end
 
